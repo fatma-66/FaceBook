90dbd2e1a18130c3f42fdc143751798e97bc635c
Fatma <ramadanfatma66@gmail.com>
2024-07-25 01:41:35 +0200

    editting deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
     images: {
       unoptimized: true,
 },
-basePath: process.env.NODE_ENV === 'production' ? '/FaceBook/' : '',
+basePath: process.env.NODE_ENV === 'production' ? '/FaceBook' : '',
 assetPrefix: process.env.NODE_ENV === 'production' ? '/FaceBook/' : '',
 };
 
