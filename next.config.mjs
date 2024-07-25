c36bd3962a514f355ce44d23298b1c2c4a935802
Fatma <ramadanfatma66@gmail.com>
2024-07-25 02:29:23 +0200

    deploying

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     images: {
       unoptimized: true,
 },
-basePath: process.env.NODE_ENV === 'production' ? '/FaceBook' : '',
-assetPrefix: process.env.NODE_ENV === 'production' ? '/FaceBook/' : '',
+// basePath: process.env.NODE_ENV === 'production' ? '/FaceBook' : '',
+// assetPrefix: process.env.NODE_ENV === 'production' ? '/FaceBook/' : '',
 };
 
 export default nextConfig;
